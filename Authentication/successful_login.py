7ad903c903552e3fb68a220b01fe92da2c874e08
ekaterinakorniichuk <korniichuk.ea@gmail.com>
2024-02-22 10:51:52 -0800

    Remove #comments

--- Authentication/successful_login.py
+++ Authentication/successful_login.py
@@ -7,19 +7,15 @@ def test_successful_login():
         browser = playwright.chromium.launch()
         page = browser.new_page()
 
-        # Выполнение входа с корректными учетными данными
+
         login(page, SWAG_BASE_URL, username, password)
 
-        # Проверка заголовка страницы после успешного входа
+
         page_title = page.title()
         assert page_title == "Swag Labs"
-
-        # Проверка наличия продуктов на странице после успешного входа
         assert page.inner_html(".inventory_list") != ""
-
-        # Закрытие браузера
         browser.close()
 
-# Запуск теста
+
 if __name__ == "__main__":
     test_successful_login()
