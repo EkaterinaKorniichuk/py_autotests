aeaa6c3e3848a466a05336ee456994dd54e88956
ekaterinakorniichuk <korniichuk.ea@gmail.com>
2024-02-26 13:27:35 -0800

    Add try/ expect construction to product_list_checking_test.py

--- Authentication/product_list_checking_test.py
+++ Authentication/product_list_checking_test.py
@@ -8,3 +8,11 @@ def test_product_list_checking():
         page = browser.new_page()
 
         page.goto(Inventory_URL)
+
+try:
+    if page.inner_text('.inventory_item') != '':
+        print("Product list displayed correctly")
+    else:
+        print("No products displayed on the screen")
+except Exception as e:
+    print("An error occurred:", e)
