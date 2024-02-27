76e1c85cbc84759e7b4a761d6bf45a6fdfef4ee4
ekaterinakorniichuk <korniichuk.ea@gmail.com>
2024-02-27 12:45:08 -0800

    1. Changed the test case 2. Changed the code 3. Add login data from page_actions.py 4. Add assert page.url 5. Add assert page.inner_text

---
+++ Authentication/product_list_test_validcr.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import sync_playwright
+from credentials import username, password, SWAG_BASE_URL, Inventory_URL
+from page_actions import login
+
+def test_product_list_():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch()
+        page = browser.new_page()
+
+        login(page, SWAG_BASE_URL, username, password, Inventory_URL)
+
+        assert page.url == 'https://www.saucedemo.com/inventory.html'
+        inventory_container_text = page.inner_text('.inventory_list')
+        assert page.inner_text('.inventory_item') != '', 'No products displayed on the screen'
+
+        browser.close()
+
+
+
