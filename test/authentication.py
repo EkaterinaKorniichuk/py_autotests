b37dbabd990736d3705287b6f2741f03febf03b6
ekaterinakorniichuk <korniichuk.ea@gmail.com>
2024-02-27 19:13:57 -0800

    Changed old empty_fields test to authentication.py

--- test/authentication.py
+++ test/authentication.py
@@ -1,30 +1,23 @@
 from playwright.sync_api import sync_playwright
-from credentials import empty_username, password, SWAG_BASE_URL
-from page_actions import login
+from util.credentials import empty_password, empty_username, SWAG_BASE_URL, username, password
+from util.page_actions import login
 
-
-   # when
-def test_login_successful():
+def test_login_failed_when_empty_username():
   with sync_playwright() as playwright:
-    browser = playwright.chromium.launch()
+    # given [these prerequisites]
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
     page = browser.new_page()
 
+    # when [I do this]
     login(page, SWAG_BASE_URL, empty_username, password)
 
-    #then
+    # then [I check this]
     error_message = page.text_content('.error-message-container h3')
     assert "Epic sadface: Username and password do not match any user in this service" in error_message
     print("Authentication error message displayed successfully.")
     browser.close()
 
-#empty_password test
-
-from playwright.sync_api import sync_playwright
-from credentials import empty_password, username, SWAG_BASE_URL
-from page_actions import login
-
-   # when
-def test_login_successful():
+def test_login_failed_when_empty_password():
   with sync_playwright() as playwright:
     browser = playwright.chromium.launch()
     page = browser.new_page()
