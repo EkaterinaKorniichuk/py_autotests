0ffffbeeea5e942c30fd50842f25b4a904f6003b
ekaterinakorniichuk <korniichuk.ea@gmail.com>
2024-02-28 14:04:34 -0800

    Add Successful_addition_of_an_item_to_the_shopping_cart.py

--- Product List/product_sorting_by_name_test.py
+++ Product List/product_sorting_by_name_test.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 from util.credentials import username, password, SWAG_BASE_URL, Inventory_URL
 from util.page_actions import login
 
-def test_product_list():
+def test_sorting_product_az():
     with sync_playwright() as playwright:
         # given
         browser = playwright.chromium.launch(headless=False, slow_mo=500)
@@ -16,9 +16,18 @@ def test_product_list():
         select_container = page.locator("button#product_sort_container")
 
 
-        page.locator("div.right_component").highlight()
-        page.locator("div.right_component:visible").highlight()
-        page.locator("div.right_component:visible a:text('Name (A to Z')").highlight()
+        #page.locator("div.right_component").highlight()
+        #page.locator("div.right_component:visible").highlight()
+        #page.locator("div.right_component:visible a:text('Name (A to Z')").highlight()
+
+        dropdown_button = page.wait_for_selector('.product_sort_container')
+        dropdown_button.click()
+
+        option_name_az = page.wait_for_selector('option[value="az"]')
+        option_name_az.click()
+
+
+
 
 
 
