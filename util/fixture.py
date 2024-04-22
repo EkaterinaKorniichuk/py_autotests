1af958a8e152f4577dcf43b0766fdf063a9b34a9
ekaterinakorniichuk <korniichuk.ea@gmail.com>
2024-04-22 15:45:19 -0700

    changed @pytest.fixture

--- util/fixture.py
+++ util/fixture.py
@@ -4,6 +4,9 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="session")
 def browser():
     with sync_playwright() as playwright:
+        print("Launching browser...")
         browser = playwright.chromium.launch(headless=False, slow_mo=500)
         yield browser
+        print("Closing browser...")
         browser.close()
+
